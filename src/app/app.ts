f448e85e9fbf8851f14773b4f7ca1f18d67aa854
Igor Henrique <ihenrique@sp.senai.br>
2026-01-05 10:52:26 -0300

    criando header e menu de navegação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit} from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { CardHeader } from "./commons/components/card-header/card-header";
+import { map, filter, Subscription } from 'rxjs';
+import { MenuBar } from './commons/components/menu-bar/menu-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardHeader, MenuBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('barber-shop-ui');
+export class App implements OnInit, OnDestroy{
+  title = ('barber-shop-ui');
+
+  private routeSubscription?: Subscription;
+
+  constructor(
+    private readonly router: Router, 
+    private readonly activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.routeSubscription =this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.getRouteTitle(this.activatedRoute))
+    ).subscribe(title => this.title = title);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription){
+      this.routeSubscription.unsubscribe();
+    }
+  }
+
+    private getRouteTitle(route: ActivatedRoute): string {
+    let child = route;
+    while (child.firstChild) {
+      child = child.firstChild;
+    }
+    return child.snapshot.data['title'] || 'Barber-Shop-ui';
+  }
 }
--- src/app/commons/components/menu-bar/menu-bar.ts
+++ src/app/commons/components/menu-bar/menu-bar.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule],
   templateUrl: './menu-bar.html',
   styleUrl: './menu-bar.scss',
 })
 export class MenuBar {
 
+  constructor(private readonly router: Router){}
+
+  navigateTo(path: string){
+    this.router.navigate([path]);
+  }
 }
